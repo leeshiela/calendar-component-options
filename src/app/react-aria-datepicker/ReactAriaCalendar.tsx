21c46387cb027f971edd8af930b904927bad9887
Shiela Lee <shiela.lee@chase.com>
2025-07-28 09:02:28 -0400

    Customized calendar with css styling

--- src/app/react-aria-datepicker/ReactAriaCalendar.tsx
+++ src/app/react-aria-datepicker/ReactAriaCalendar.tsx
@@ -6,37 +6,43 @@ import {
   Calendar,
   CalendarCell,
   CalendarGrid,
+  CalendarGridHeader,
+  CalendarHeaderCell,
   Heading,
 } from "react-aria-components";
 
 export default function AriaCalendar() {
-  const { value, setValue } = useState<CalendarDate>();
+  const { date, setDate } = useState<Date>();
   let now = today(getLocalTimeZone());
 
   return (
     <Flex direction="column" alignItems="center" mt={6}>
-      <Text fontSize="xl">React Aria Calendar</Text>
-      <Flex>
-        <Text fontSize="3xl" mt={6} mb={6}>
-          Reserve a Table
-        </Text>
-        <Text>{value ? `${value}` : ``}</Text>
-      </Flex>
+      <Text fontSize="2xl" mb={6}>
+        React Aria Calendar
+      </Text>
       <Calendar
         aria-label="Reservation Date"
-        onChange={setValue}
+        onChange={setDate}
         minValue={now}
         maxValue={now.add({ months: 3 })}
+        firstDayOfWeek="sun"
       >
         <header>
           <Heading />
-          <Flex gap={6}>
+
+          <Flex gap={10}>
             <Button slot="previous">&lt;</Button>
             <Button slot="next">&gt;</Button>
           </Flex>
         </header>
         <CalendarGrid>{(date) => <CalendarCell date={date} />}</CalendarGrid>
       </Calendar>
+      <Flex>
+        <Text fontSize="3xl" mt={6} mb={6}>
+          Reserve a Table
+        </Text>
+        <Text>{date ? `${date}` : ``}</Text>
+      </Flex>
     </Flex>
   );
 }
