898809ab375491948936d84fb4d02f8cd3de48b8
Shiela Lee <shiela.lee@chase.com>
2025-07-29 08:07:31 -0400

    Imported styles and modified code from previous project

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,26 @@
 import { Container, Flex } from "@chakra-ui/react";
+import DatePickerCalendar from "./react-datepicker/components/DatePickerCalendar";
 
 export function App() {
   return (
     <Container
       display="flex"
-      maxW="600px"
       mx="auto"
       justifyContent="center"
-      flexDir="row"
+      flexDir="column"
       pt="10"
     >
-      <Flex fontSize="3xl">Calendar Component Options</Flex>
+      <Flex fontSize="3xl" justifyContent="center">
+        Calendar Component Options
+      </Flex>
+      <Flex
+        mt={6}
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <DatePickerCalendar />
+      </Flex>
     </Container>
   );
 }
---
+++ src/app/react-datepicker/components/DatePickerCalendar.tsx
@@ -0,0 +1,61 @@
+import { Box, Button, Flex, Heading, Text } from "@chakra-ui/react";
+import React, { forwardRef, useState } from "react";
+import DatePicker from "react-datepicker";
+import dayjs from "dayjs";
+import "react-datepicker/dist/react-datepicker.css";
+
+const DatePickerCalendar = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [calendarOpen, setCalendarOpen] = useState(false);
+
+  const formatDate = (date: Date) => dayjs(date).format("MMM D").toUpperCase();
+
+  const handleChange = (date: Date) => setSelectedDate(date);
+
+  const renderDateSelect = () => {
+    return (
+      <Button type="button" variant="totallyUnstyled" color={"gray.300"}>
+        <Heading fontSize={"lg"} as="div" mr={2.5} color={"black"}>
+          {formatDate(selectedDate)}
+        </Heading>
+        {/* <ChevronDownIcon
+          size={2.5}
+          transform={calendarOpen ? "rotate(180deg)" : "rotate(0deg)"}
+          transition="transform 0.1s ease"
+        /> */}
+      </Button>
+    );
+  };
+
+  return (
+    <DatePicker
+      selected={selectedDate}
+      dateFormat="MMM d"
+      useWeekdaysShort
+      onChange={handleChange}
+      customInput={renderDateSelect()}
+      onCalendarOpen={() => setCalendarOpen(true)}
+      onCalendarClose={() => setCalendarOpen(false)}
+      enableTabLoop={false}
+      showPopperArrow={false}
+      calendarClassName="calendar"
+      popperClassName="popper"
+      minDate={new Date()}
+      weekDayClassName={(_date: Date) => "weekdays"}
+      renderCustomHeader={({ date }) => (
+        <Box
+          className="customHeader"
+          backgroundColor="white"
+          display="flex"
+          alignItems="flex-start"
+          paddingLeft={2}
+        >
+          <Heading fontSize={"lg"} as="div">
+            {dayjs(date).format("MMMM YYYY")}
+          </Heading>
+        </Box>
+      )}
+    />
+  );
+};
+export default DatePickerCalendar;
