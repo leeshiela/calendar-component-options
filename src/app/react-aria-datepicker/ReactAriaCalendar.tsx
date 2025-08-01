babd86b7860663565368470841563c24ee5c165e
Shiela Lee <shiela.lee@chase.com>
2025-08-01 12:17:42 -0400

    Fixed arrows and fixed labeling on top to populate date selected

--- src/app/react-aria-datepicker/ReactAriaCalendar.tsx
+++ src/app/react-aria-datepicker/ReactAriaCalendar.tsx
@@ -1,4 +1,9 @@
-import { CalendarDate, getLocalTimeZone, today } from "@internationalized/date";
+import {
+  CalendarDate,
+  DateFormatter,
+  getLocalTimeZone,
+  today,
+} from "@internationalized/date";
 import { Flex, Text } from "@chakra-ui/react";
 import { useState } from "react";
 import {
@@ -11,38 +16,55 @@ import {
   Heading,
 } from "react-aria-components";
 
+interface OptionsType {
+  day: string | undefined;
+  month: string | undefined;
+}
+
 export default function AriaCalendar() {
-  const [date, setDate] = useState<Date>();
   let now = today(getLocalTimeZone());
+  const [date, setDate] = useState<CalendarDate | undefined>(now);
+  const options: OptionsType = { day: "numeric", month: "short" };
 
   return (
     <Flex direction="column" alignItems="center" mt={6}>
       <Text fontSize="2xl" mb={6}>
         React Aria Calendar
       </Text>
+      <Flex>
+        <Text fontSize="3xl" mt={6} mb={6}>
+          {date
+            ? `${date
+                .toDate(getLocalTimeZone())
+                .toLocaleDateString("en-US", options)}`
+            : ``}
+        </Text>
+      </Flex>
       <Calendar
         aria-label="Reservation Date"
         onChange={setDate}
         minValue={now}
-        maxValue={now.add({ months: 3 })}
         firstDayOfWeek="sun"
       >
         <header>
           <Heading />
 
           <Flex gap={10}>
-            <Button slot="previous">&lt;</Button>
-            <Button slot="next">&gt;</Button>
+            <Button slot="previous">
+              <img className="arrows" src="/arrow.svg" alt="previous-arrow" />
+            </Button>
+            <Button slot="next">
+              <img
+                className="arrows"
+                style={{ transform: "rotate(180deg)" }}
+                src="/arrow.svg"
+                alt="next-arrow"
+              />
+            </Button>
           </Flex>
         </header>
         <CalendarGrid>{(date) => <CalendarCell date={date} />}</CalendarGrid>
       </Calendar>
-      <Flex>
-        <Text fontSize="3xl" mt={6} mb={6}>
-          Reserve a Table
-        </Text>
-        <Text>{date ? `${date}` : ``}</Text>
-      </Flex>
     </Flex>
   );
 }
